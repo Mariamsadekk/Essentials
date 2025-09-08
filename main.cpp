b31493613e639cd919e30b9abbd7a83fc7204486
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-09-08 16:58:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Define the Student struct here
+
+
+// Function to input student details
+// void inputStudent(Student* s) { }
+
+// Function to update GPA using reference
+// void updateGPA(Student& s, float newGPA) { }
+
+// Function to print student details
+// void printStudent(Student s) { }
+
+int main() {
+    // Create an array of 3 students
+
+    // Use inputStudent to read their details
+
+    // Update the GPA of the second student using updateGPA
+
+    // Print all students using printStudent
+
+    return 0;
+}
